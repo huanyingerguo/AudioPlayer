c7f8945fe63972ca948a8fb01464dec484d9cbfb
sunjinglin <1021832889@qq.com>
2020-07-06 17:17:23 +0800

    重构一下文件结构

---
+++ AudioPlayer/AudioPlayer/AudioPlayer.h
@@ -0,0 +1,17 @@
+//
+//  AudioPlayer.h
+//  AudioPlayer
+//
+//  Created by sunjinglin on 2020/7/6.
+//  Copyright © 2020 Sun,Jinglin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AudioPlayer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AudioPlayer/AudioPlayer/AudioPlayer.m
@@ -0,0 +1,13 @@
+//
+//  AudioPlayer.m
+//  AudioPlayer
+//
+//  Created by sunjinglin on 2020/7/6.
+//  Copyright © 2020 Sun,Jinglin. All rights reserved.
+//
+
+#import "AudioPlayer.h"
+
+@implementation AudioPlayer
+
+@end
